6a98e419c0ba03e5d7b82ce17a52ad9e03ef7719
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-01 10:48:46 -0400

    Time: 56 ms (28.85%) | Memory: 49.1 MB (16.53%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,14 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
     let count = 0;
-  for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] !== " ") {
-        count++;
-        if (s[i-1] === " " || s[i-1] === undefined) {
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] === " " && count > 0) {
             return count;
+        } else if (s[i] !== " ") {
+            count++;
         }
     }
-  }
+    return count;
 };
