d511e3fd0ecac9110933662476ebd73b4d3a6b66
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-02 10:31:54 -0400

    Time: 55 ms (95.77%) | Memory: 53.1 MB (27.48%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -15,11 +15,7 @@ var sortedArrayToBST = function(nums) {
   if (nums.length === 0) return null;
   //find midpoint of nums
   let mid = Math.floor(nums.length / 2);
-  //make TS from nums[midpoint]
-  // let root = new TreeNode(nums[mid]);
-  // root.left = sortedArrayToBST(nums.slice(0, mid));
-  // root.right = sortedArrayToBST(nums.slice(mid + 1));
-  // return root;
+
   return new TreeNode(
     nums[mid], 
     sortedArrayToBST(nums.slice(0, mid)),
