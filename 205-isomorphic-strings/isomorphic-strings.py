17c0162a0391e98612477ce4186044a4402bfe2d
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-03 11:14:12 -0400

    Time: 43 ms (55.48%) | Memory: 16.7 MB (64.69%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,21 +1,5 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        # if len(s) is not len(t) return false
-
-        # create a map of s -> t
-        # create a map of t -> s
-        
-        # loop over s by index
-        # if s_char is not in s_t
-            # add s_char map, map to t[i]
-            # add t[i] to t_map, map to s_char
-        # else
-            # check val of s_t[s_char]
-            # if t_s[val] is not s_char
-            # return false
-        
-        # return true
-
         if len(s) != len(t):
             return False
         
