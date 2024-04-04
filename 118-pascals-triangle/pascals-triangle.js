44429800fecba081104281265af3e582652a585d
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-04 16:51:32 -0400

    Time: 67 ms (13.30%) | Memory: 52 MB (8.23%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -3,39 +3,28 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    //create [] for out
-    //loop for numRows x - start from 1, end when > than numRows
-        //create a subarray
-        //make another arr for sums = sum of each number plus prev. num
-        
-        //if curr = 1, push 1 into subarray
-        //sums = []
+    //if numRows is 0, return [[]]
 
-        //else
-            //add 1 to the beginning of subarr
-            //spread out sums and add the subarr
-            //add 1 to the end of subarr
-            //loop over the subarr, until i reaches the end
-                //add i to prev val -> push this sum into sums
-            //push subarray into out
-    //return out
-    const out = [];
-    let sums = [];
-    for (let rowNum = 1; rowNum <= numRows; rowNum++) {
+    //init result = [[1]]
+    //loop for i = 1; i < numRows
+        //init row -> []
+        //make temp arr -> append 0 to beg and end of the last row in result
+            //for j = 1; until j reaches end of temp
+            // push the sum of j and prev into row
+        //push row into the result
+    //return result
+
+    if (numRows === 0) return [[]];
+
+    let result = [[1]];
+    while (result.length < numRows) {
         let row = [];
-        
-        if (rowNum === 1) {
-            row.push(1);
-        } else {
-            row.push(1);
-            row.push(...sums);
-            row.push(1);
-            sums = [];
-            for (let i = 1; i < row.length; i++) {
-                sums.push(row[i] + row[i - 1]);
-            }
+        let temp = [0, ...result[result.length - 1], 0];
+        for (let i = 1; i < temp.length; i++) {
+            row.push(temp[i] + temp[i - 1]); 
         }
-        out.push(row);
+        result.push(row);
     }
-    return out;
+
+    return result;
 };
