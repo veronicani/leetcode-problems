0c7d385cd74ac82db629e035c9ff733d82b476d8
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-05 10:14:57 -0400

    Time: 81 ms (54.33%) | Memory: 63.5 MB (18.37%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    //loop over nums
-        //if the curr val is in the set
-            //return true
-        //add the curr val to a set
-    //return false
     const nums2 = new Set();
     for (const n of nums) {
         if (nums2.has(n)) return true;
