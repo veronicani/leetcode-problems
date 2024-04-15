908838f3edb4bcfb5dcc93cd4b0827baefae2992
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-15 11:18:22 -0400

    Time: 143 ms (44.77%) | Memory: 57.7 MB (97.70%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -3,25 +3,19 @@
  * @return {number[]}
  */
 var replaceElements = function(arr) {
-    //init greatest to be first val
-    //loop over arr, until reaching the last el
-        //if the greatest val is the curr val
-            //check the vals to the right of the curr -- find the new greatest val, for loop
-            //set greatest to be next val
-        //replace curr val with the greatest val
-    //replace the last el with -1
-    let greatest = arr[0];
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] === greatest) {
-            greatest = arr[i + 1];
-            for (let j = i + 1; j < arr.length; j++) {
-                if (arr[j] > greatest) {
-                    greatest = arr[j];
-                }
-            }
-        }
-        arr[i] = greatest;
+    //init max = -1;
+    let max = -1;
+    let right = -1;
+    //loop from the end of the arr to the begin
+    for (let i = arr.length - 1; i >= 0; i--) {
+        //store the num to the right of curr val
+        //reassign max to be max between the prev. max & the num to the right
+        max = Math.max(max, right);
+        right = arr[i];
+        //replace the curr val with max
+        arr[i] = max;
     }
-    arr[arr.length - 1] = -1;
+
     return arr;
+
 };
