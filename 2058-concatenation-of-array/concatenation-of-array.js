977d3c12c86f2ebe0373175479102432dafada8e
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-15 10:33:03 -0400

    Time: 81 ms (5.53%) | Memory: 51.8 MB (68.69%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.js
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -4,10 +4,11 @@
  */
 var getConcatenation = function(nums) {
     const ans = [];
-    while (ans.length < 2 * nums.length) {
-        for (let num of nums) {
+    for (let count = 0; count < 2; count++) {
+            for (let num of nums) {
             ans.push(num);
         }
     }
+
     return ans;
 };
