a8bbb90cabe522d519ac2c9bf831eef4db6aeba7
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-17 10:14:31 -0400

    Time: 60 ms (20.08%) | Memory: 48.9 MB (47.94%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    //initiate count = 0
-    //loop over each num, from index
-      //if num not val -> replace nums[count] with num, increment count
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
-      if (nums[i] !== val) {
-        nums[count] = nums[i];
-        count++;
-      }
+        if (nums[i] === val) {
+            nums[i] = '_';
+        } else {
+            count++;
+        }
     }
+    nums = nums.sort();
     return count;
 };
