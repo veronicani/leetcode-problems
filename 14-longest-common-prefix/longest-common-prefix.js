6df376a002ec091f8da2d6f28b16c9715b8d62e6
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-17 09:45:33 -0400

    Time: 61 ms (30.94%) | Memory: 52.2 MB (5.87%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -4,13 +4,8 @@
  */
 var longestCommonPrefix = function(strs) {
     let out = "";
-    let shortestLen = +Infinity;
-    
-    for (let str of strs) {
-        if (str.length < shortestLen) shortestLen = str.length;
-    }
 
-    for (let i = 0; i < shortestLen; i++) {
+    for (let i = 0; i < strs[0].length; i++) {
         for (let str of strs) {
             if (str[i] !== strs[0][i]) return out;
         }
