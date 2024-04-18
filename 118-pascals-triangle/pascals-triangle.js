c1763b43b7cd1a3d186f7737aff8b15f9116aa02
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-18 09:07:45 -0400

    Time: 59 ms (50.19%) | Memory: 51.7 MB (12.30%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -3,14 +3,21 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
-	const res = [[1]];
+    if (numRows === 0) return [[]];
+    let tri = [[1]];
 
-	for (let i = 1; i < numRows; i++) {
-        res[i] = [];
-		for (let k = 0; k < i + 1; k++) {
-			res[i][k] = (res[i - 1][k] || 0) + (res[i - 1][k - 1] || 0);
-		}
-	}
-
-    return res;
+    for (let r = 1; r < numRows; r++) {
+        let temp = [0, ...tri[r - 1], 0];
+        let row = [];
+        for (let i = 1; i < temp.length; i++) {
+            row.push(temp[i - 1] + temp[i]);
+        }
+        tri.push(row);
+    }
+    return tri;
 };
+
+//nRows = 3
+//tri = [[1], [1, 1], [1, 2, 1]]
+//temp = [0, 1, 1, 0]
+//row = [1, 2, 1]
