4e226f007bbbdb919ad3f54cd61130c931a1998d
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-19 09:11:21 -0400

    Time: 35 ms (89.64%) | Memory: 16.7 MB (94.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,24 +1,22 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+        if len(s) != len(s):
             return False
-        
+
         s_t = {}
         t_s = {}
-        
-        for i, s_char in enumerate(s):
-            t_char = t[i]
 
-            if s_char in s_t:
-                if s_t[s_char] != t_char:
+        for i in range(len(s)):
+            sChar = s[i]
+            tChar = t[i]
+            if sChar in s_t:
+                if s_t[sChar] != tChar:
                     return False
-            
+            elif tChar in t_s:
+                if t_s[tChar] != sChar:
+                    return False
             else:
-                if t_char in t_s:
-                    return False
-
-                s_t[s_char] = t_char
-                t_s[t_char] = s_char
-
-        return True
+                s_t[sChar] = tChar
+                t_s[tChar] = sChar
         
+        return True
