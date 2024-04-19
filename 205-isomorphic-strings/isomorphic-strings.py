cf3bbdbd3121943cc3fde63c2cc378cdafc47d57
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-19 09:22:48 -0400

    Time: 41 ms (63.01%) | Memory: 16.9 MB (25.57%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -9,14 +9,12 @@ class Solution:
         for i in range(len(s)):
             sChar = s[i]
             tChar = t[i]
-            if sChar in s_t:
-                if s_t[sChar] != tChar:
+
+            if ((sChar in s_t and s_t[sChar] != tChar) or 
+                (tChar in t_s and t_s[tChar] != sChar)):
                     return False
-            elif tChar in t_s:
-                if t_s[tChar] != sChar:
-                    return False
-            else:
-                s_t[sChar] = tChar
-                t_s[tChar] = sChar
+            
+            s_t[sChar] = tChar
+            t_s[tChar] = sChar
         
         return True
