0f09f71fbb8198b0a5f6e75f2704ea5db4d0c0a6
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-22 09:50:53 -0400

    Time: 55 ms (88.23%) | Memory: 52.1 MB (36.39%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -5,26 +5,16 @@
  */
 var nextGreaterElement = function(nums1, nums2) {
     const ans = [];
-    let lastLoggedIdx = 0; //keep track of last val found @ nums2
-    let matchIdx = 0;
     const nums2Idx = {};
+    
+    for (let n = 0; n < nums2.length; n++) {
+        const n2 = nums2[n]
+        nums2Idx[n2] = n;
+    }
+
     for (let i = 0; i < nums1.length; i++) {
-        //if n1 is in nums2Idx, return lookup of n1
-        let n1 = nums1[i]
-        if (nums2Idx[n1] !== undefined) {
-            matchIdx = nums2Idx[n1];
-        } else {
-        //loop over nums2, start from lastLogged
-            for (let j = lastLoggedIdx; j < nums2.length; j++) {
-                let n2 = nums2[j];
-                nums2Idx[n2] = j;
-                lastLoggedIdx++;
-                if (nums2Idx[n1] !== undefined) {
-                    matchIdx = nums2Idx[n1];
-                    break;
-                }
-            }
-        }
+        const n1 = nums1[i];
+        const matchIdx = nums2Idx[n1];
         //find greater val than curr val of nums1 @ nums2
         for (let j = matchIdx + 1; j < nums2.length; j++) {
             if (nums2[j] > nums1[i]) {
