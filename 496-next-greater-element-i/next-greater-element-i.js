4a85913a69453a65b7a881d2a663ba4eb06581e4
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-22 09:42:46 -0400

    Time: 63 ms (52.00%) | Memory: 51.4 MB (78.41%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,18 +4,28 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    //let ans = [];
-    //iterate over nums1
-        //find the idx of matching val to curr1
-        //loop over nums2 starting from idx of match
-            //if curr2 g8 than curr1
-                //push the val into ans
-        //push -1 into ans
-    
-    //return ans
     const ans = [];
+    let lastLoggedIdx = 0; //keep track of last val found @ nums2
+    let matchIdx = 0;
+    const nums2Idx = {};
     for (let i = 0; i < nums1.length; i++) {
-        let matchIdx = nums2.findIndex(n2 => n2 === nums1[i]);
+        //if n1 is in nums2Idx, return lookup of n1
+        let n1 = nums1[i]
+        if (nums2Idx[n1] !== undefined) {
+            matchIdx = nums2Idx[n1];
+        } else {
+        //loop over nums2, start from lastLogged
+            for (let j = lastLoggedIdx; j < nums2.length; j++) {
+                let n2 = nums2[j];
+                nums2Idx[n2] = j;
+                lastLoggedIdx++;
+                if (nums2Idx[n1] !== undefined) {
+                    matchIdx = nums2Idx[n1];
+                    break;
+                }
+            }
+        }
+        //find greater val than curr val of nums1 @ nums2
         for (let j = matchIdx + 1; j < nums2.length; j++) {
             if (nums2[j] > nums1[i]) {
                 ans.push(nums2[j]);
