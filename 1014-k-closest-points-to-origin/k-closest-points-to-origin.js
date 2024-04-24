1a6c4079a38f5ef0b6a7f2e0885c4fb2316e68d1
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-24 10:50:35 -0400

    Time: 299 ms (16.19%) | Memory: 69.4 MB (23.94%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -1,5 +1,5 @@
-var calcEucDistFrom0 = function ([x, y]) {
-    return Math.sqrt(Math.pow((x - 0), 2) + Math.pow((y - 0), 2));
+var euclideanDist = function([x, y]) {
+    return Math.sqrt((x - 0) ** 2 + (y - 0) ** 2);
 }
 
 /**
@@ -8,22 +8,17 @@ var calcEucDistFrom0 = function ([x, y]) {
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    //iterate over the points, add the idx and dist of each to map - (dist, idx)
-    let distances = [];
-    for (let i = 0; i < points.length; i++) {
-        distances.push([calcEucDistFrom0(points[i]), i]);
+    const maxPQ = new MaxPriorityQueue();
+    for (p of points) {
+        let dist = euclideanDist(p);
+        if (maxPQ.size() < k) {
+            maxPQ.enqueue(p, dist);
+        } else if (dist < maxPQ.front().priority) {
+            maxPQ.dequeue();
+            maxPQ.enqueue(p, dist);
+        }
     }
-    //sort the map, from small -> large
-    distances.sort((a, b) => (a[0] - b[0]));
-    
-    //for k times
-        //push val of map[i] into the out
-    const res = [];
-    for (let j = 0; j < k; j++) {
-        const currD = distances[j];
-        const pIdx = currD[1];
-        res.push(points[pIdx]);
-    }
+
+    const res = maxPQ.toArray().map(v => v.element);
     return res;
 };
-
