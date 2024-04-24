0142a9c600b67563e942e2b4debd08ad90827371
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-24 15:55:54 -0400

    Time: 1998 ms (5.29%) | Memory: 68.7 MB (5.05%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let buyDate = 0;
+    let maxProfit = 0;
+    let b = 0;
+    let s = b + 1;
+
+    while (s < prices.length) {
+        if (prices[b] > prices[s]) {
+            b = s;
+        } else {
+            let currProfit = prices[s] - prices[b];
+            if (currProfit > maxProfit) {
+                buyDate = b;
+                maxProfit = currProfit;
+            }
+        }
+        s++;
+        console.log("bval=", prices[b], "sval=", prices[s]);
+    }
+
+    return maxProfit;
+};
