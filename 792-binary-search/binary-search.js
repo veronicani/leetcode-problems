872b120c7d837efe27136b9267dedba1f39050e4
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-26 12:59:06 -0400

    Time: 63 ms (20.64%) | Memory: 52 MB (49.88%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -7,7 +7,7 @@ var search = function(nums, target) {
     let max = nums.length - 1;
     let min = 0;
     while (max >= min) {
-        let m = Math.floor((min + max) / 2);
+        let m = Math.floor(min + (max - min) / 2);
         if (nums[m] === target) return m;
         if (nums[m] > target) max = m - 1;
         else if (nums[m] < target) min = m + 1;
