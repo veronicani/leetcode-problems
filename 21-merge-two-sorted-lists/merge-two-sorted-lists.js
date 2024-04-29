a5ea930da87943b444b5ae2fcfd3b70c736493c9
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-04-28 20:44:36 -0400

    Time: 58 ms (77.00%) | Memory: 51.2 MB (83.90%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,28 +11,17 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if (list1 === null) return list2;
-    if (list2 === null) return list1;
-    
-    let head = list1.val <= list2.val ? list1 : list2;
-    let curr = head;
+    if (!list1) return list2;
+    if (!list2) return list1;
 
-    if (head === list1) list1 = list1.next;
-    else list2 = list2.next;
+    let sorted = new ListNode();
 
-    while (list1 && list2) {
-        if (list1.val <= list2.val) {
-            curr.next = list1;
-            list1 = list1.next;
-        } else {
-            curr.next = list2;
-            list2 = list2.next;
-        }
-        curr = curr.next;
+    if (list1.val <= list2.val) {
+        sorted.next = list1;
+        sorted.next.next = mergeTwoLists(list1.next, list2);
+    } else {
+        sorted.next = list2;
+        sorted.next.next = mergeTwoLists(list1, list2.next);
     }
-
-    if (list1) curr.next = list1;
-    else curr.next = list2;
-
-    return head;
+    return sorted.next;
 };
