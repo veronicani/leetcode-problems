dd1b79a6f81ba3b2e101475f6fffc3324ef6372e
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-01 11:16:02 -0400

    Time: 52 ms (60.10%) | Memory: 49.1 MB (46.49%) - LeetSync

--- 100-same-tree/same-tree.js
+++ 100-same-tree/same-tree.js
@@ -13,10 +13,6 @@
  */
 var isSameTree = function(p, q) {
     if (!p && !q) return true;
-    if (p && !q) return false;
-    if (!p && q) return false;
-    if (p.val !== q.val) return false;
-    if (isSameTree(p.left, q.left) === false) return false;
-    if (isSameTree(p.right, q.right) === false) return false;
-    return true;
+    if (!p || !q || p.val !== q.val) return false;
+    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
 };
