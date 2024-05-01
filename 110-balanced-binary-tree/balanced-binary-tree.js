62ef2b048a49511f0aef73a8257e4ccaa7068333
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-01 10:16:29 -0400

    Time: 70 ms (28.90%) | Memory: 54.6 MB (33.48%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.js
+++ 110-balanced-binary-tree/balanced-binary-tree.js
@@ -12,14 +12,14 @@
  */
 var isBalanced = function(root) {
     const isBalanced = [true];
-    var balanced = function(root, balanced) {
+    var balanced = function(root) {
         if (!root) return 0;
-        const left = balanced(root.left, balanced);
-        const right = balanced(root.right, balanced);
+        const left = balanced(root.left);
+        const right = balanced(root.right);
         if (Math.abs(left - right) > 1) isBalanced[0] = false;
         return (Math.max(left, right) + 1);
     }
-    balanced(root, balanced);
+    balanced(root);
     return isBalanced[0];
 };
 
