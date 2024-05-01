fc8fe259174f809c9221b4f944369a336f3745a9
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-01 08:56:01 -0400

    Time: 66 ms (51.22%) | Memory: 55.5 MB (89.61%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -12,15 +12,15 @@
  */
 var diameterOfBinaryTree = function(root) {
     let max = [0];
-    diameterOfTree(root, max);
+    length(root, max);
     return max[0];
     
 };
 
-var diameterOfTree = function(root, max) {
+var length = function(root, max) {
     if (!root) return 0;
-    const left = diameterOfTree(root.left, max);
-    const right = diameterOfTree(root.right, max);
+    const left = length(root.left, max);
+    const right = length(root.right, max);
 
     max[0] = Math.max(max, left + right);
     return Math.max(left, right) + 1;
