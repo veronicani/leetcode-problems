509167186f38005e1bbd5d80b9ac79e4e78b04de
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-05 19:44:23 -0400

    Time: 70 ms (75.51%) | Memory: 59 MB (53.77%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,15 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0;
-    let b = 0;
-    let s = b + 1;
+    let max = 0;
+    let buy = 0;
+    let sell = 1;
 
-    while (s < prices.length) {
-        if (prices[b] > prices[s]) {
-            b = s;
-        } else {
-            let currProfit = prices[s] - prices[b];
-            maxProfit = Math.max(currProfit, maxProfit);
-        }
-        s++;
+    while (sell < prices.length) {
+        let profit = prices[sell] - prices[buy];
+        max = Math.max(max, profit);
+        if (prices[sell] < prices[buy]) buy = sell;
+        sell++;
     }
-
-    return maxProfit;
+    return max;
 };
