00ac62ac3e4d91cd2196786f36f008efe3bad410
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-06 10:47:09 -0400

    Time: 1546 ms (5.05%) | Memory: 59.2 MB (5.14%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    let max = 0;
+    let l = 0;
+    let r = 0;
+    let count = {}; //stores the count of chars in curr window
+    let maxCharCount = 0;
+    while (r < s.length) {
+        count[s[r]] = count[s[r]] === undefined ? 1 : count[s[r]] + 1;
+        console.log('count=', count);
+        maxCharCount = Math.max(count[s[r]], maxCharCount);
+        console.log('maxCharCount=', maxCharCount);
+        let windowLen = r - l + 1;
+        while (windowLen - maxCharCount > k) {
+            count[s[l]]--;
+            maxCharCount = Math.max(...Object.values(count));
+            l++;
+            windowLen = r - l + 1;
+        }
+        max = Math.max(windowLen, max);
+        console.log('max=', max);
+        r++;
+    }
+    return max;
+};
