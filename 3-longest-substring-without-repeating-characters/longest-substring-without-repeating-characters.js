8695006e0b2002171de114e0184d3e8136c02c4e
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-05 20:49:09 -0400

    Time: 82 ms (67.98%) | Memory: 52.2 MB (95.73%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -6,16 +6,15 @@ var lengthOfLongestSubstring = function(s) {
     let max = 0;
     let b = 0;
     let e = 0;
+    let chars = new Set();
+
     while (e < s.length) {
-        let sub = s.slice(b, e + 1);
-        let chars = new Set(sub);
-        while (chars.size < sub.length) {
+        while (chars.has(s[e])) {
+            chars.delete(s[b]);
             b++;
-            sub = s.slice(b, e + 1);
-            chars = new Set(sub);
         }
-        let len = e - b + 1;
-        max = Math.max(len, max);
+        chars.add(s[e]);
+        max = Math.max(chars.size, max);
         e++;
     }
     return max;
