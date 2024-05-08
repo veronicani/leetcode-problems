43d6fe51072a1de666f5878c1454983afe9350b0
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-08 16:39:35 -0400

    Time: 78 ms (28.08%) | Memory: 57.5 MB (60.92%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (sum < 0) {
+            sum = 0;    
+        }
+        sum += nums[i];
+        max = Math.max(sum, max);
+    }
+    return max;
+};
