fc03afc29e2c865e161779763598d5ccc406c2da
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-07 21:48:29 -0400

    Time: 55 ms (33.04%) | Memory: 49.3 MB (7.24%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.js
+++ 54-spiral-matrix/spiral-matrix.js
@@ -3,42 +3,40 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
+    const numVals = matrix.length * matrix[0].length;
     let left = 0;
     let right = matrix[0].length - 1;
     let top = 0;
     let bottom = matrix.length - 1;
     const res = [];
-    let x = 0;
-    let y = 0;
-    while (res.length < matrix.length * matrix[0].length) {
+
+    while (res.length < numVals) {
         //traverse right from topleft -> topright
-        for (let x = left; x <= right; x++) {
-            let curr = matrix[top][x];
-            if (curr !== null) res.push(curr);
-            matrix[top][x] = null;
+        for (let x = left; x <= right && res.length < numVals; x++) {
+            res.push(matrix[top][x]);
         }
         top++;
+
         //traverse down from topright -> bottomright
-        for (let y = top; y <= bottom; y++) {
-            let curr = matrix[y][right];
-            if (curr !== null) res.push(curr);
-            matrix[y][right] = null;
+        for (let y = top; y <= bottom && res.length < numVals; y++) {
+            res.push(matrix[y][right]);
         }
         right--;
+
         //traverse left from bottomright -> bottomleft;
-        for (let x = right; x >= left; x--) {
-            let curr = matrix[bottom][x];
-            if (curr !== null) res.push(curr);
-            matrix[bottom][x] = null;
+        for (let x = right; x >= left && res.length < numVals; x--) {
+            res.push(matrix[bottom][x]);
         }
         bottom--;
+
+
         //traverse up from bottomleft -> topleft;
-        for (let y = bottom; y >= top; y--) {
-            let curr = matrix[y][left];
-            if (curr !== null) res.push(curr);
-            matrix[y][left] = null;
+        
+        for (let y = bottom; y >= top && res.length < numVals; y--) {
+            res.push(matrix[y][left]);
         }
         left++;
+        
         console.log('res=', res);
     }
     return res;
