92aeafbe6a75b7cf861dbb49a7a78410e149febf
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-08 09:54:33 -0400

    Time: 447 ms (6.27%) | Memory: 61.1 MB (56.11%) - LeetSync

--- 817-design-hashmap/design-hashmap.js
+++ 817-design-hashmap/design-hashmap.js
@@ -1,98 +1,39 @@
-class LLNode {
-    constructor(key, val) {
-        this.key = key;
-        this.val = val;
-        this.next = null;
-        this.prev = null;
-    }
-}
-
-class LL {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    findNode(key) {
-        let curr = this.head;
-        while (curr) {
-            if (curr.key === key) return curr;
-            curr = curr.next;
-        }
-        return null;
-    }
-    
-    append(key, val) {
-        let node = this.findNode(key);
-        if (node) {
-            node.val = val;
-            return;
-        }
-        let newNode = new LLNode(key, val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-            this.tail = newNode;
-        }
-    }
-
-    remove(key) {
-        let node = this.findNode(key);
-        if (!node) return null;
-        let prev = node.prev;
-        let next = node.next;
-        if (!prev) this.head = next;
-        if (!next) this.tail = prev;
-        
-        if (prev) prev.next = next;
-        if (next) next.prev = prev;
-        return true;
-    }
-}
-
 class MyHashMap {
-    constructor(size = 0) {
-        this.array = this._createArray(size);
+    constructor() {
+        this.map = [];
     }
 
-    _createArray(size) {
-        const arr = [];
-        for (let i = 0; i < size; i++) {
-            arr.push(null);
-        }
-        return arr;
-    }
-    
     _hashToIdx(key) {
-        return key;
+        //hashes the key to an idx, returns idx
+        return key.length % 2;
     }
 
     put(key, val) {
         const idx = this._hashToIdx(key);
-        let ll = this.array[idx];
-        if (!ll) {
-            ll = new LL();
-            this.array[idx] = ll;
+        this.map[idx] = this.map[idx] || [];
+        if (!this.map[idx].length) this.map[idx].push([key, val]);
+        else {
+            let i = this.map[idx].findIndex(([k, v]) => k === key);
+            if (i === -1) this.map[idx].push([key, val]);
+            else this.map[idx][i] = [key, val];
         }
-        ll.append(key, val);
     }
-
-    remove(key, val) {
-        const idx = this._hashToIdx(key);
-        const ll = this.array[idx];
-        if (!ll) return -1;
-        if (ll.remove(key) === null) return -1;
-    }
-
     get(key) {
         const idx = this._hashToIdx(key);
-        const ll = this.array[idx];
-        if (!ll) return -1;
-        let node = ll.findNode(key);
-        if (!node) return -1;
-        return node.val;
+        if (!this.map[idx]) return -1;
+        
+        let item = this.map[idx].find(([k, v]) => k === key);
+        if (!item) return -1;
+        let [k, v] = item;
+        return v;
+        
+    }
+    remove(key) {
+        const idx = this._hashToIdx(key);
+        if (!this.map[idx]) return -1;
+        
+        let i = this.map[idx].findIndex(([k, v]) => k === key);
+        if (i === -1) return -1;
+        this.map[idx].splice(i, 1);
     }
 }
