0a84b867a341c6cca8fcc6615066ec4595081138
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-08 10:27:57 -0400

    Time: 169 ms (49.18%) | Memory: 61.7 MB (43.23%) - LeetSync

--- 817-design-hashmap/design-hashmap.js
+++ 817-design-hashmap/design-hashmap.js
@@ -4,36 +4,22 @@ class MyHashMap {
     }
 
     _hashToIdx(key) {
-        //hashes the key to an idx, returns idx
-        return key.length % 2;
+        return key;
     }
 
     put(key, val) {
         const idx = this._hashToIdx(key);
-        this.map[idx] = this.map[idx] || [];
-        if (!this.map[idx].length) this.map[idx].push([key, val]);
-        else {
-            let i = this.map[idx].findIndex(([k, v]) => k === key);
-            if (i === -1) this.map[idx].push([key, val]);
-            else this.map[idx][i] = [key, val];
-        }
+        this.map[idx] = val;
     }
+
     get(key) {
         const idx = this._hashToIdx(key);
-        if (!this.map[idx]) return -1;
-        
-        let item = this.map[idx].find(([k, v]) => k === key);
-        if (!item) return -1;
-        let [k, v] = item;
-        return v;
-        
+        return (this.map[idx] !== undefined ? this.map[idx] : -1);
     }
+
     remove(key) {
         const idx = this._hashToIdx(key);
-        if (!this.map[idx]) return -1;
-        
-        let i = this.map[idx].findIndex(([k, v]) => k === key);
-        if (i === -1) return -1;
-        this.map[idx].splice(i, 1);
+        if (this.map[idx] === undefined) return -1;
+        this.map[idx] = undefined;
     }
 }
