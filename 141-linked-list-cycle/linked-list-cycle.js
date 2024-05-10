0eeb41cf160514884f494b486cbc92983db13f16
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-10 14:12:10 -0400

    Time: 67 ms (40.87%) | Memory: 53.9 MB (22.00%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -11,15 +11,9 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    //create a set
-    //loop through the ll
-        //if it isn't in the set, add it
-        //if it is in the set -> return true
-    //return false;
-    const seen = new Set();
     while (head) {
-        if (seen.has(head)) return true;
-        seen.add(head);
+        if (head.seen) return true;
+        head.seen = true;
         head = head.next;
     }
     return false;
