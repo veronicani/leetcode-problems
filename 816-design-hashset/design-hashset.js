37c2515ab967b06bf12540a694d8a9abdecb9fbf
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-13 10:13:00 -0400

    Time: 979 ms (5.09%) | Memory: 60.9 MB (79.43%) - LeetSync

--- 816-design-hashset/design-hashset.js
+++ 816-design-hashset/design-hashset.js
@@ -1,17 +1,51 @@
+class LLNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class MyHashSet {
     constructor() {
-        this.set = [];
+        this.set = Array.from(10 ** 4);
+    }
+
+    hashKey(key) {
+        return key % this.set.length;
     }
 
     add(key) {
-        this.set[key] = key;
-    }
-
-    contains(key) {
-        return this.set[key] !== undefined;
+        const hashIdx = this.hashKey(key);
+        if (!this.set[hashIdx]) this.set[hashIdx] = new LLNode(0);
+        let curr = this.set[hashIdx];
+        while(curr.next) {
+            if (curr.next.val === key) return;
+            curr = curr.next;
+        }
+        curr.next = new LLNode(key);
     }
 
     remove(key) {
-        if (this.set[key]) this.set[key] = undefined;
+        const hashIdx = this.hashKey(key);
+        let curr = this.set[hashIdx];
+        if (!curr) return;
+        while (curr.next) {
+            if (curr.next.val === key) {
+                curr.next = curr.next.next;
+                return;
+            }
+            curr = curr.next;
+        }
+    }
+
+    contains(key) {
+        const hashIdx = this.hashKey(key);
+        let curr = this.set[hashIdx];
+        if (!curr) return false;
+        while (curr.next) {
+            if (curr.next.val === key) return true;
+            curr = curr.next;
+        }
+        return false;
     }
 }
