70dcb610d41883f524aa3d4c958cf46af6cdf941
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-15 19:29:59 -0400

    Time: 172 ms (26.91%) | Memory: 57.2 MB (78.18%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.js
+++ 303-range-sum-query-immutable/range-sum-query-immutable.js
@@ -1,16 +1,22 @@
-class NumArray {
-    constructor(nums) {
-        this.nums = [...nums];
-    }
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = [...nums];
+};
 
-    sumRange(left, right) {
-        let sum = 0;
-        for (let i = left; i <= right; i++) {
-            sum += this.nums[i];
-        }
-        return sum;
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(left, right) {
+    let sum = 0;
+    for (let i = left; i <= right; i++) {
+        sum += this.nums[i];
     }
-}
+    return sum;
+};
 
 /** 
  * Your NumArray object will be instantiated and called as such:
