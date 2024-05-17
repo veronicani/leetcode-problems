12d2c652959407e9df1219a2cc026420d4a4b961
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-17 15:00:09 -0400

    Time: 180 ms (9.49%) | Memory: 60.9 MB (7.00%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -3,20 +3,31 @@
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-    let perimeter = 0;
+    let seen = new Set();
+    
+    var findPerimeter = function(y, x) {
+        if (y < 0 || y >= grid.length ||
+            x < 0 || x >= grid[0].length || 
+            grid[y][x] === 0) {
+                return 1;
+            }
+        //
+        if (seen.has(`${y},${x}`)) return 0;
+        seen.add(`${y},${x}`);
+        let perimeter = 0;
+        perimeter += findPerimeter(y - 1, x);
+        perimeter += findPerimeter(y + 1, x);
+        perimeter += findPerimeter(y, x - 1);
+        perimeter += findPerimeter(y, x + 1);
+
+        return perimeter;
+    }
     for (let y = 0; y < grid.length; y++) {
         for (let x = 0; x < grid[0].length; x++) {
-            let sq = grid[y][x];
-            if (sq === 0) continue; 
-            if (y !== 0) {
-                if (!grid[y - 1][x]) perimeter++;
-            } else perimeter++;
-            if (y !== grid.length - 1) {
-                if (!grid[y + 1][x]) perimeter++;
-            } else perimeter++;
-            if (!grid[y][x + 1]) perimeter++;
-            if (!grid[y][x - 1]) perimeter++;
-        }           
+            if (grid[y][x] === 1) {
+                return findPerimeter(y, x);
+            }
+        }
     }
-    return perimeter;
+    return 0;
 };
