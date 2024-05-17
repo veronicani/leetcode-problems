b15240b62eb3c764bf8f8f7f8049b74fa80c368f
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-17 12:19:58 -0400

    Time: 79 ms (45.40%) | Memory: 61.2 MB (37.80%) - LeetSync

--- 134-gas-station/gas-station.js
+++ 134-gas-station/gas-station.js
@@ -4,19 +4,21 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    let gasGain = gas.reduce((a, b) => a + b, 0);
-    let gasCost = cost.reduce((a, b) => a + b, 0);
-    if (gasCost > gasGain) return -1;
+    let tGas = 0;
+    let tCost = 0
 
     let tank = 0;
     let start = 0;
     for (let i = 0; i < gas.length; i++) {
+        tGas += gas[i];
+        tCost += cost[i];
+        
         tank += gas[i] - cost[i];
         if (tank < 0) {
             tank = 0;
             start = i + 1;
         }
     }
-    return start;
+    return tGas >= tCost ? start : -1;
 };
 
