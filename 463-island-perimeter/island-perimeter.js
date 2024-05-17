5be354ccb19f8caca6cee74bd8ab853bbac5710b
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-17 13:58:40 -0400

    Time: 127 ms (65.57%) | Memory: 58.1 MB (52.92%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -7,7 +7,6 @@ var islandPerimeter = function(grid) {
     for (let y = 0; y < grid.length; y++) {
         for (let x = 0; x < grid[0].length; x++) {
             let sq = grid[y][x];
-            console.log('y=', y, 'x=', x, 'sq=', sq);
             if (sq === 0) continue; 
             if (y !== 0) {
                 if (!grid[y - 1][x]) perimeter++;
@@ -17,7 +16,6 @@ var islandPerimeter = function(grid) {
             } else perimeter++;
             if (!grid[y][x + 1]) perimeter++;
             if (!grid[y][x - 1]) perimeter++;
-            console.log('peri=', perimeter);
         }           
     }
     return perimeter;
