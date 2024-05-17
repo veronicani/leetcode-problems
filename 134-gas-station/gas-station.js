25dc4b18660b56f59edc55b22fbba488daa430de
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-17 12:32:45 -0400

    Time: 88 ms (18.32%) | Memory: 61.3 MB (37.80%) - LeetSync

--- 134-gas-station/gas-station.js
+++ 134-gas-station/gas-station.js
@@ -4,19 +4,20 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    let tGas = 0;
-    let tCost = 0
+    let tGas = 0; //if the total cost is > than total gas, 
+    let tCost = 0 //we can't complete the circuit
 
     let tank = 0;
-    let start = 0;
-    for (let i = 0; i < gas.length; i++) {
-        tGas += gas[i];
-        tCost += cost[i];
+    let start = 0; 
+    //let i be a mark for each station
+    for (let i = 0; i < gas.length; i++) { 
+        tGas += gas[i]; //calc the total gas and total cost as we loop
+        tCost += cost[i]; 
         
-        tank += gas[i] - cost[i];
-        if (tank < 0) {
-            tank = 0;
-            start = i + 1;
+        tank += gas[i] - cost[i]; //the tank will accumulate this val
+        if (tank < 0) { //if the tank ever falls below 0
+            tank = 0;   
+            start = i + 1; //our starting point needs to be the next station
         }
     }
     return tGas >= tCost ? start : -1;
