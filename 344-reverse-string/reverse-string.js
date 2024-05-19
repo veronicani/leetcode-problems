862d61b7c7d78d03269dcb2c1e5243ccbc631479
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-19 11:52:58 -0400

    Time: 75 ms (71.21%) | Memory: 57.4 MB (27.66%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -6,9 +6,7 @@ var reverseString = function(s) {
     let start = 0;
     let end = s.length -1;
     while (start < end) {
-      let temp = s[start];
-      s[start] = s[end];
-      s[end] = temp;
+      [s[start], s[end]] = [s[end], s[start]];
       start++;
       end--;
     }
