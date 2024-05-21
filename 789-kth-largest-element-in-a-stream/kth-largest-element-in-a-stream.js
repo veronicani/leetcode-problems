701ac382e58af3f60409d9b3d2a018f502620852
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-21 14:18:28 -0400

    Time: 468 ms (27.61%) | Memory: 59.6 MB (69.81%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.js
@@ -1,22 +1,91 @@
-class KthLargest {
-    constructor(k, nums) {
-        this.k = k;
-        this.minHeap = new MinPriorityQueue();
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
 
+class MPriorityQueue {
+    constructor(capacity, nums){
+        this.capacity = capacity;
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+        
         for (const n of nums) {
-            this.add(n);
+            this.enqueue(n);
         }
     }
 
-    add(val, { minHeap } = this) {
-        if (minHeap.size() < this.k) {
-            minHeap.enqueue(val);
-            return minHeap.front().element;
+    _findNextNode(node) {
+        let curr = this.head;
+        while (curr) {
+            if (node.val < curr.val) return curr;
+            curr = curr.next;
         }
-        if (val > minHeap.front().element) {
-            minHeap.dequeue(minHeap.front().element);
-            minHeap.enqueue(val);
+        return null;
+    }
+
+    enqueue(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            let nextNode = this._findNextNode(newNode);
+            if (!nextNode) {
+                newNode.prev = this.tail;
+                this.tail.next = newNode;
+                this.tail = newNode;
+            } else {
+                let prev = nextNode.prev;
+                if (!prev) {
+                    this.head = newNode;
+                } else {
+                    newNode.prev = prev;
+                    prev.next = newNode;
+                }
+                nextNode.prev = newNode;
+                newNode.next = nextNode;   
+            }
         }
-        return minHeap.front().element;
+        this.length++;
+        while (this.length > this.capacity) {
+            this.dequeue();
+        }
+    }
+
+    dequeue() {
+        let toRemove = this.head;
+        let nextHead = this.head.next;
+        toRemove.next = null;
+        if (nextHead) {
+            nextHead.prev = null;    
+            this.head = nextHead;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+        this.length--;
+    }
+
+    findTop() {
+        return this.head.val;
+    }
+}
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+class KthLargest {
+    constructor(k, nums) {
+        this.pQ = new MPriorityQueue(k, nums);
+    }
+
+    add(val) {
+        this.pQ.enqueue(val);
+        return this.pQ.findTop();
     }
 }
