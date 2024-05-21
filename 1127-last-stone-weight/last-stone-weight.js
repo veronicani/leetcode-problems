f05d09d0a2a2392060d7d734ff784a4b7bb5fa17
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-21 15:00:47 -0400

    Time: 69 ms (18.78%) | Memory: 50.7 MB (50.11%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -10,10 +10,8 @@ var lastStoneWeight = function(stones) {
     while (stonesOrdered.size() > 1) {
         let first = stonesOrdered.dequeue().element;
         let second = stonesOrdered.dequeue().element;
-        console.log('first=', first, 'second=', second);
         let result = first - second;
         if (result > 0) stonesOrdered.enqueue(result);
-        console.log('stonesO=', stonesOrdered);
     }
     return stonesOrdered.size() === 1 ? stonesOrdered.dequeue().element : 0;
 };
