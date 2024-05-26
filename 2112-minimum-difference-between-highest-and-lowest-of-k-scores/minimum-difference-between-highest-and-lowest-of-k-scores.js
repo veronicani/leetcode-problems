eab00331962632234e1cfb805422766c6bded1bd
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-26 19:39:47 -0400

    Time: 193 ms (5.22%) | Memory: 58.1 MB (6.09%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+    if (nums.length <= 1) return 0;
+
+    nums.sort((a, b) => a - b);
+    let minDiff = +Infinity;
+    let l = 0;
+    while (l <= nums.length - k) {
+        let r = l + k - 1;
+        let min = nums[l];
+        let max = nums[r];
+        console.log('min=', min, 'max=', max);
+        minDiff = Math.min(minDiff, max - min);
+        l++;
+    }
+    return minDiff;
+};
