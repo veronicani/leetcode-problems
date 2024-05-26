c3dac5bf3762146a5acf7da56564a73a1577de56
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-26 19:59:24 -0400

    Time: 52 ms (64.31%) | Memory: 48.8 MB (66.31%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -1,16 +1,23 @@
-var merge = function (nums1, m, nums2, n) {
-  let n1 = m - 1;
-  let n2 = n - 1;
-  let nL = nums1.length - 1;
-
-  while (n2 >= 0) {
-    if (nums1[n1] > nums2[n2]) {
-      nums1[nL] = nums1[n1];
-      n1--;
-    } else {
-      nums1[nL] = nums2[n2];
-      n2--;
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let n1 = m - 1;
+    let n2 = n - 1;
+    let nL = nums1.length - 1;
+    while (n2 >= 0) {
+        if (nums1[n1] > nums2[n2]) {
+            nums1[nL] = nums1[n1];
+            n1--;
+        } else {
+            nums1[nL] = nums2[n2];
+            n2--;
+        }
+        nL--;
     }
-    nL--;
-  }
-}
+    return nums1;
+};
