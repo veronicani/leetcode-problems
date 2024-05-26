6833e36aba678bcb13043e6f1454cff07a3c0023
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-26 19:52:18 -0400

    Time: 53 ms (62.62%) | Memory: 51.1 MB (7.39%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,16 +4,15 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let res = '';
+    let res = [];
     let i = 0;
     while (i < word1.length && i < word2.length) {
-        res += word1[i];
-        res += word2[i];
+        res.push(word1[i]);
+        res.push(word2[i]);
         i++;
     }
-    if (i < word1.length) {
-        res += word1.slice(i);
-    } else res += word2.slice(i);
+    res.push(...word1.slice(i));
+    res.push(...word2.slice(i));
     
-    return res;
+    return res.join('');
 };
