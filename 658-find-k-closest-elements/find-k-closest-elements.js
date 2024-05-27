77e04babffd0b13b637c963b160f883668b91c99
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-27 10:24:51 -0400

    Time: 74 ms (75.57%) | Memory: 55.3 MB (79.01%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.js
+++ 658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,51 +5,13 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let left = 0;
-    let right = arr.length - 1;
-    let cVal = arr[0];
-    let cIdx = 0;
-
-    while (left <= right) {
-        let m = Math.floor((left + right) / 2);
-        let currDiff = Math.abs(x - arr[m]);
-        let cDiff = Math.abs(x - cVal);
-        if (currDiff < cDiff || 
-            (currDiff === cDiff && arr[m] < cVal)){
-                cVal = arr[m];
-                cIdx = m;
-            }
-        
-        if (arr[m] > x) {
-            right = m - 1;
-        } else if (arr[m] < x) {
-            left = m + 1;
-        } else {
-            break;
-        }
+    let l = 0;
+    let r = arr.length - 1;
+    while (r - l >= k) {
+        let diffL = Math.abs(x - arr[l]);
+        let diffR = Math.abs(x - arr[r]);
+        if (diffL > diffR) l++;
+        else r--;
     }
-
-    let l = cIdx;
-    let r = cIdx;
-    console.log("cIdx =", cIdx);
-    console.log("lr val =", arr[l]);
-    
-    for (let i = 1; i < k; i++) {
-        let diffNextL = Math.abs(x - arr[l - 1]);
-        let diffNextR = Math.abs(x - arr[r + 1]);
-
-        if (l === 0) {
-            r++;
-        } else if (r === arr.length - 1) {
-            l--;
-        } else if (diffNextL <= diffNextR) {
-            l--;
-        } else {
-            r++;
-        }
-        console.log("i=", i, "lval=", arr[l], "rval=", arr[r]);
-    }
-
     return arr.slice(l, r + 1);
-
 };
