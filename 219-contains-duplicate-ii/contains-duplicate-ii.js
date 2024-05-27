f74a76391e0fd950fd1e17cdc060d65f374c8dba
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-27 09:59:29 -0400

    Time: 89 ms (73.98%) | Memory: 63.1 MB (88.41%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -6,15 +6,13 @@
 var containsNearbyDuplicate = function(nums, k) {
     let seen = new Set();
     let l = 0;
-    let r = 0;
-    while (r < nums.length) {
-        while (seen.size > k) {
+    for (let r = 0; r < nums.length; r++) {
+        while (r - l > k) {
             seen.delete(nums[l]);
             l++;
         }
         if (seen.has(nums[r])) return true;
         seen.add(nums[r]);
-        r++;
     }
     return false;
 };
