440f37c7ee0e0bac2bb688a2d32200093e44a779
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-26 20:29:36 -0400

    Time: 83 ms (28.93%) | Memory: 53.8 MB (75.39%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,15 +3,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            for (let j = i + 1; j < nums.length; j++) {
-                if (nums[j] !== 0) {
-                    [nums[i], nums[j]] = [nums[j], nums[i]];
-                    break;
-                }
-            }
+    let i = 0;
+    let j = 1;
+    while (j < nums.length) {
+        while (nums[i] !== 0 && j < nums.length - 1) {
+            i++;
+            j++;
         }
+        while (nums[j] === 0 && j < nums.length - 1) {
+            j++;
+        }
+        if (nums[j] !== 0 && nums[i] === 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+        }
+        i++;
+        j++;
     }
     return nums;
 };
+//0 0 1 0 3 12
+//1 0 0 0 3 12
+//1 3 0 0 0 12
+//1 3 12 0 0 0       
