5901f9f310038769e5888644267c9b698174fb98
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-28 09:35:56 -0400

    Time: 52 ms (59.07%) | Memory: 49.3 MB (18.80%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -6,12 +6,11 @@
 var searchInsert = function(nums, target) {
     let l = 0;
     let r = nums.length - 1;
-    let mid;
     while (l <= r) {
-        mid = Math.floor((l + r) / 2);
+        let mid = Math.floor((l + r) / 2);
         if (nums[mid] === target) return mid;
         else if (nums[mid] > target) r = mid - 1;
-        else if (nums[mid] < target) l = mid + 1;
+        else l = mid + 1;
     }
     return l;
 };
