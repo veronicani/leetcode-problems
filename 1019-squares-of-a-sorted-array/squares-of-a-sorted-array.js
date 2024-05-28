f0a1de919b45847b13daf283a27264f6369a2c04
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-28 11:36:10 -0400

    Time: 82 ms (66.23%) | Memory: 56.3 MB (53.19%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,7 +3,40 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let squares = nums.map(n => n ** 2);
-    squares.sort((a, b) => a - b);
-    return squares;
+    //find the smallest abs val
+    //start l and r at smallestIdx
+    //while (l >= 0 && r < nums.length)
+        //compare nums[l] and nums[r]
+    //if (nums[l]), push square into out
+    //else push square of nums[r] into out
+    let smallestAbs = +Infinity;
+    let smallestIdx = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (Math.abs(nums[i]) < smallestAbs) {
+            smallestAbs = Math.abs(nums[i]);
+            smallestIdx = i;  
+        }
+    }
+    let l = smallestIdx;
+    let r = l + 1;
+    let res = [];
+    while (l >= 0 && r < nums.length) {
+        if (Math.abs(nums[l]) <= Math.abs(nums[r])) {
+            res.push(nums[l] ** 2);
+            l--;
+        } else {
+            res.push(nums[r] ** 2);
+            r++;
+        }
+    }
+    while (nums[l] !== undefined) {
+        res.push(nums[l] ** 2);
+        l--;
+    }
+    while (nums[r] !== undefined) {
+        res.push(nums[r] ** 2);
+        r++;
+    }
+    
+    return res;
 };
