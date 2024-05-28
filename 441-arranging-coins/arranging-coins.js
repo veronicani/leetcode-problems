426d443376f3c427160cf63efe0511f10dcf249e
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-28 10:46:10 -0400

    Time: 88 ms (57.51%) | Memory: 53.5 MB (45.16%) - LeetSync

--- 441-arranging-coins/arranging-coins.js
+++ 441-arranging-coins/arranging-coins.js
@@ -7,7 +7,7 @@ var arrangeCoins = function(n) {
     let high = n;
     while (low <= high) {
         let row = Math.floor((low + high) / 2);
-        let coinsNeeded = (row / 2) * (row + 1);
+        let coinsNeeded = (row / 2) * (row + 1); //Gauss' formula
         if (n === coinsNeeded) return row;
         if (n < coinsNeeded) high = row - 1;
         else low = row + 1;
