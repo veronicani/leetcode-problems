e378d0c157ec8449b143de015f1d8aadf77b07a6
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-28 10:14:41 -0400

    Time: 152 ms (5.69%) | Memory: 59.6 MB (5.57%) - LeetSync

--- 441-arranging-coins/arranging-coins.js
+++ 441-arranging-coins/arranging-coins.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var arrangeCoins = function(n) {
-    let row = 1;
-    while (n >= row) {
-        n -= row;
-        row++;
-    }
-    return (row - 1);
+    return removeCoins(1, n);
 };
+
+var removeCoins = function(row, coins) {
+    if (coins < row) return row - 1;
+    return (removeCoins(row + 1, coins - row));
+}
