2a3f6afc4d8a6652c7aead9ce7794a4a3cafbb39
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-05-29 09:16:42 -0400

    Time: 57 ms (7.56%) | Memory: 16.8 MB (61.46%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,15 +1,20 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
         scores = []
+        sum = 0
         for o in operations:
             try:
                 scores.append(int(o))
+                sum += int(o)
             except ValueError:
                 pass
             if o == '+':
                 scores.append(scores[-1] + scores[-2])
+                sum += scores[-1]
             elif o == 'D':
                 scores.append(scores[-1] * 2)
+                sum += scores[-1]
             elif o == 'C':
+                sum -= scores[-1]
                 scores.pop()
-        return sum(scores)
+        return sum
