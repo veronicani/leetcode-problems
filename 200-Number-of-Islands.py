28bb9a95532c39e06f39e987e6a846e79165ae0f
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-07-24 10:52:47 -0400

    [200] [Time Beats: 53.79%] [Memory Beats: 48.06%] - LeetPush

---
+++ 200-Number-of-Islands.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        def markIsland(coords):
+            y, x = coords
+            if (y < 0 or y >= len(grid)) or (x < 0 or x >= len(grid[0])):
+                return
+            if grid[y][x] == '0' or grid[y][x] == 'x':
+                return
+            grid[y][x] = 'x'
+            coords = [(y - 1, x), (y + 1, x), (y, x + 1), (y, x - 1)]
+            for c in coords:
+                markIsland(c)
+        
+        for y in range(len(grid)):
+            for x in range(len(grid[y])):
+                if grid[y][x] == '1':
+                    count += 1
+                    markIsland((y, x))
+        return count
