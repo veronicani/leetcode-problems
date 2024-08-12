d0b1e0195bdb8dedc596e702ba7074bb06a9118b
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-08-12 11:33:13 -0400

    [130] [Time Beats: 30.91%] [Memory Beats: 52.04%] - LeetPush

--- 130-Surrounded-Regions.py
+++ 130-Surrounded-Regions.py
@@ -4,35 +4,30 @@ class Solution:
         Do not return anything, modify board in-place instead.
         \\\
         ROWS, COLS = len(board), len(board[0])
-        def protect(y, x):
-            coords = [(y+1, x), (y-1, x), (y, x-1), (y, x+1)]
-            if (
-                y < 0
-                or y > ROWS - 1
-                or x < 0
-                or x > COLS - 1
-                or board[y][x] != 'O'
-            ):
-                return
-            board[y][x] = '#' 
-            for y, x in coords:
-                protect(y, x)
 
-        # protect unsurrounded regions O -> #
+        def protect(y, x):
+            if (y not in range(ROWS) or x not in range(COLS) or board[y][x] != 'O'):
+                return
+            board[y][x] = '$'
+            delta = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+            for yd, xd in delta:
+                yy, xx = yd + y, xd + x
+                protect(yy, xx)
+                    
+        
         for r in range(ROWS):
             for c in range(COLS):
-                 # check for Os at edge of board
-                if board[r][c] == 'O' and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                if r in [0, ROWS - 1] or c in [0, COLS - 1]:
                     protect(r, c)
         
         for r in range(ROWS):
             for c in range(COLS):
-                # every O that hasn't been protected can now be surrounded
-                if board[r][c] == 'O':
+                if board[r][c] == 'O': 
                     board[r][c] = 'X'
-                # changed protected regions # back into O
-                elif board[r][c] == '#':
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == '$': 
                     board[r][c] = 'O'
-        
-        
         return board
+        
