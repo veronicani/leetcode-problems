1b51f3bee6055e929cb5450b81659c7c91649ddb
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-08-13 09:57:51 -0400

    [199] [Time Beats: 88.03%] [Memory Beats: 67.26%] - LeetPush

--- 199-Binary-Tree-Right-Side-View.py
+++ 199-Binary-Tree-Right-Side-View.py
@@ -1,4 +1,3 @@
-import collections
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,19 +7,15 @@ import collections
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
-        to_visit = collections.deque()
-        if root:
-            to_visit.append(root)
-        
-        while to_visit:
-            level = []
-            for i in range(len(to_visit)):
-                node = to_visit.popleft()
-                level.append(node.val)
-                if node.left:
-                    to_visit.append(node.left)
-                if node.right:
-                    to_visit.append(node.right)
-            res.append(level[len(level) - 1])
-        
+        if not root:
+            return res
+        queue = collections.deque([root])
+        while queue:
+            res.append(queue[-1].val)
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
         return res
