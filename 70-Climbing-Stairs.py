c1267ce1e70791cb6aa80d67787eabcbb4ba3821
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-08-16 11:59:01 -0400

    [70] [Time Beats: 5.93%] [Memory Beats: 57.09%] - LeetPush

--- 70-Climbing-Stairs.py
+++ 70-Climbing-Stairs.py
@@ -1,14 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {0: 1, 1: 1}
-        
-        def climb(n: int, memo: dict[int, int]) -> int:
-            if n == 0 or n == 1:
-                return 1
-            if n not in memo:
-                memo[n] = climb(n - 1, memo) + climb(n-2, memo)
-            
-            return memo[n]
+        if n == 0 or n == 1:
+            return 1
 
-        climb(n, memo)
-        return memo[n]
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
