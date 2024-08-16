70483f5860a506fd8d04d7a69a8189c577fc2967
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2024-08-16 13:09:45 -0400

    [21] [Time Beats: 63.70%] [Memory Beats: 16.06%] - LeetPush

---
+++ 21-Merge-Two-Sorted-Lists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    let sorted = new ListNode(0);
+    let cur = sorted;
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            cur.next = list1;
+            list1 = list1.next;
+        } else {
+            cur.next = list2;
+            list2 = list2.next;
+        }
+        cur = cur.next;
+    }
+    if (list1) cur.next = list1;
+    else cur.next = list2;
+    return sorted.next;
+};
