a251993c492b85612f8e70f5841bb988227d8035
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2025-03-07 09:19:45 -0500

    Time: 38 ms (58.25%) | Memory: 17.7 MB (48.63%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -2,7 +2,9 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         res = []
         i = 0
-        while i < len(word1) and i < len(word2):
+        w1, w2 = len(word1), len(word2)
+
+        while i < w1 and i < w2:
             res.append(word1[i])
             res.append(word2[i])
             i += 1
