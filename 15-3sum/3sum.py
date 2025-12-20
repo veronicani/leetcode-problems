bb5b3d3cbb45912ac7254e2a22899cf0dbe42b78
Veronica Ni <53886347+veronicani@users.noreply.github.com>
2025-12-20 17:43:30 -0500

    Time: 547 ms (70.74%) | Memory: 20.1 MB (95.87%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,42 +2,22 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
-        for i in range(len(nums) - 1):
-            if (i > 0 and nums[i] == nums[i - 1]):
+        i = 0
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            a = nums[i]
-            if a > 0:
-                break
-            l = i + 1
-            r = len(nums) - 1
+            l, r = i + 1, len(nums) - 1
             while l < r:
-                b = nums[l]
-                c = nums[r]
-                threeSum = a + b + c
-                if threeSum < 0:
-                    l += 1
-                elif threeSum > 0:
+                total = nums[i] + nums[l] + nums[r]
+                if total > 0:
                     r -= 1
-                else:
-                    res.append([a, b, c])
+                elif total < 0:
                     l += 1
-                    while (nums[l] == nums[l - 1] and l < r):
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    # inc l until it points to diff num
+                    l += 1
+                    while nums[l] == nums[l-1] and l < r:
                         l += 1
         return res
-
-
-# [-4, -1, -1, 0, 1, 2, 5]
-# return if i is greater than 0
-# set l - 1 ahead of i
-# set r at the end
-    # while l < r
-    # if the sum of 3 vals < 0
-        # move l up
-    # if sum 3 vals > 0
-        # move r down
-    # if equal 0
-        # push 3 vals into res
-        # move l up
-        # while l is same as l - 1, move l up
-
-
+                
